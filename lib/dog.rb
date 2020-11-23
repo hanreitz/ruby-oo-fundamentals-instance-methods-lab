54b040e272a6c1439d4a170be2613994baab1876
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2020-11-23 13:31:57 -0500

    commit tree 2cc33d93cdd98e81ee0ea913c1bd8133bb2f9f4d

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+#something about the actual dogs
+#bark instance method
+    def bark
+        puts "Woof!"
+    end
+#sit instance method
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person
+#something about people
+#talk instance method
+    def talk
+        puts "Hello World!"
+    end
+#walk instance method
+    def walk
+        puts "The Person is walking"
+    end
+end
